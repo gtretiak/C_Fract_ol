65e18e5b5082b6f5877e177b0eee782d99a12baa
Georgii Tretiakov <george22021988@gmail.com>
2025-07-16 17:00:17 +0100

    Update events.c

--- Sources/events.c
+++ Sources/events.c
@@ -15,9 +15,9 @@
 int	ft_close(t_mlx *fractol)
 {
 	ft_putstr_fd(3);
-	mlx_destroy_image(fractol->mlx, fractol->img.img);
-	mlx_destroy_window(fractol->mlx, fractol->win);
-	mlx_destroy_display(fractol->mlx);
+	mlx_destroy_image(fractol->mlx, fractol->img.img); // frees the image from memory
+	mlx_destroy_window(fractol->mlx, fractol->win); // closes the window
+	mlx_destroy_display(fractol->mlx); // destroys the display (connection)
 	free(fractol->mlx);
 	exit (0);
 }
@@ -29,11 +29,11 @@ int	ft_handle_mouse(int button, int x, int y, t_mlx *fractol)
 
 	x_init = x;
 	y_init = y;
-	if (button == 5)
-		fractol->zoom *= 1.1;
-	else if (button == 4)
+	if (button == 5) // scroll up - zoom in
+		fractol->zoom *= 1.1; 
+	else if (button == 4) // scroll down - zoom out
 		fractol->zoom *= 0.9;
-	ft_render(fractol);
+	ft_render(fractol); // redraw with new zoom level
 	return (0);
 }
 
@@ -42,7 +42,7 @@ int	ft_handle_key(int keysym, t_mlx *fractol)
 	if (keysym == XK_Escape || keysym == XK_q || keysym == XK_x)
 		ft_close(fractol);
 	else if (keysym == XK_Left)
-		fractol->shift_x -= (0.5 * fractol->zoom);
+		fractol->shift_x -= (0.5 * fractol->zoom); // pan left
 	else if (keysym == XK_Right)
 		fractol->shift_x += (0.5 * fractol->zoom);
 	else if (keysym == XK_Up)
@@ -50,9 +50,9 @@ int	ft_handle_key(int keysym, t_mlx *fractol)
 	else if (keysym == XK_Down)
 		fractol->shift_y -= (0.5 * fractol->zoom);
 	else if (keysym == XK_KP_Add || keysym == XK_plus || keysym == XK_equal)
-		fractol->iter += 10;
+		fractol->iter += 10; // increase details
 	else if (keysym == XK_KP_Subtract || keysym == XK_minus)
-		fractol->iter -= 10;
-	ft_render(fractol);
+		fractol->iter -= 10; // decrease details
+	ft_render(fractol); // re-render with update state
 	return (0);
 }
